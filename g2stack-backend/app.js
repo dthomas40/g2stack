3efe57949d415b4be219092449998ef05517b313
learnskill11 <55130492+learnskill11@users.noreply.github.com>
2021-05-13 01:29:10 -0700

    app port set herokuapp server

--- g2stack-backend/app.js
+++ g2stack-backend/app.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const connectDB = require('./config/db');
-var cors = require('cors');
+const express = require("express");
+const connectDB = require("./config/db");
+var cors = require("cors");
 
 // routes
-const books = require('./routes/api/books');
+const books = require("./routes/api/books");
 
 const app = express();
 
@@ -16,11 +16,13 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get("/", (req, res) => res.send("Hello world!"));
 
 // use Routes
-app.use('/api/books', books);
+app.use("/api/books", books);
 
 const port = process.env.PORT || 8082;
+
+app.set("port", port);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
